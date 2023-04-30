93aa3fcc2cf83f92d7d493dc88d41c5be53f0eea
HoangNgocAnhDO <dohoangngocanh1710@gmail.com>
2023-04-30 15:27:00 +0200

    finish

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -3,31 +3,47 @@ import './contact.css'
 import {MdOutlineEmail} from 'react-icons/md'
 import {RiMessengerLine} from 'react-icons/ri'
 import {BsWhatsapp} from 'react-icons/bs'
+import { useRef } from 'react';
+import emailjs from 'emailjs-com'
 
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_41fb5bm', 'template_vf9ph49', form.current, '-7VjNOM4yY01Kk-oK')
+    e.target.reset()
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
+
   return (
     <section id='contact'>
       <h5>Get in Touc</h5>
       <h2>Contact Me</h2>
 
-      <div className='container contact_container'>
+      <div className="container contact_container">
         <div className="contact__options">
           <article className="contact__option">
-            <MdOutlineEmail />
+            <MdOutlineEmail className='contact__option-icon'/>
             <h4>Email</h4>
             <h5>dohoangngocanh1710@gmail.com</h5>
             <a href="mailto:dohoangngocanh1710@gmail.com" target='_blank'>Send a message</a>
           </article>
 
           <article className="contact__option">
-            <RiMessengerLine />
+            <RiMessengerLine className='contact__option-icon'/>
             <h4>Messenger</h4>
             <h5>dohoangngocanh</h5>
             <a href="https://m.me/CNMCSCGVM" target='_blank'>Send a message</a>
           </article>
 
           <article className="contact__option">
-            <BsWhatsapp />
+            <BsWhatsapp className='contact__option-icon'/>
             <h4>Whatsapp</h4>
             <h5>dohoangngocanh</h5>
             <a href="https://api.whatsapp.com/send?phone=+33624318209" target='_blank'>Send a message</a>
@@ -35,7 +51,7 @@ const Contact = () => {
 
         </div>
         {/* END OF CONTACT OPTION */}
-        <form action="">
+        <form ref={form} onSubmit={sendEmail}>
           <input type="text" name='name' placeholder='Your Full Name' required/>
           <input type="text" name='email' placeholder='Your Email' required />
           <textarea name="message" rows="7" placeholder='Your Message' required></textarea>
